dc16c49a752556f91bd9e5e1b4d29999005c43d0
Christian Klettner <christian@bizscisolve.com.au>
2023-10-04 10:28:01 +1100

    addd Nino indices

--- nino.R
+++ nino.R
@@ -1,3 +1,4 @@
+
 library(tsibble)
 library(tidyverse)
 library(feasts)
@@ -8,17 +9,15 @@ path <- "https://www.cpc.ncep.noaa.gov/data/indices/sstoi.indices"
 
 download.file(path, destfile="temp.data")
 
-colnames <- read_fwf("temp.data", 
-                     col_positions=fwf_empty("temp.data"), 
-                     n_max=1) %>% gather()
-
 data <- read_fwf("temp.data", skip=1, na = c("99.9")) %>% 
-  set_names(c("Year", "Month", "NINO1+2", "NINO1+2.ANOM", "NINO3", "NINO3.ANOM", "NINO4", "NINO4.ANOM", "NINO3+4", "NINO3+4.ANOM")) %>% 
+  set_names(c("Year", "Month", "NINO1+2.LEVEL", "NINO1+2.ANOM", "NINO3.LEVEL", "NINO3.ANOM", "NINO4.LEVEL", "NINO4.ANOM", 
+              "NINO3+4.LEVEL", "NINO3+4.ANOM")) %>% 
   mutate(Date  = tsibble::make_yearmonth(Year, Month)) %>% 
   select(-Year, -Month) %>% 
-  pivot_longer(cols=-Date, names_to = "NinoMeasure", values_to="Anomaly") %>% 
-  as_tsibble(index=Date, key=NinoMeasure) %>% 
-  model(STL(Anomaly ~ trend() + season(period=12))) %>% 
+  pivot_longer(cols=-Date, names_to = c("NinoModel", "Measure"), names_sep="\\.", values_to="Value") %>% 
+  pivot_wider(id_cols=c(Date, NinoModel), names_from = Measure, values_from = Value) %>% 
+  as_tsibble(index=Date, key=NinoModel) %>% 
+  model(STL(ANOM ~ trend() + season(period=12))) %>% 
   components()
 
 write_csv(data %>% mutate(Date=as.Date(Date)), "data/nino.csv")
